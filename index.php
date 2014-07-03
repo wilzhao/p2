3f725eb515fdcd9a6086e12571764ef9beb06122
wilzhao <wzhaomt454@gmail.com>
2014-07-03 19:57:51 -0400

    updated index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<title> xkcd Password Generator </title>
 	<meta charset = "utf-8">
-	<link rel = "stylesheet" href = "style.css" type = "css">
+	<link rel = "stylesheet" href = "style.css">
 </head>
 
 <body>
