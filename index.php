b55955a6b144ee58b8dfa10e7d5df2225a320182
wilzhao <wzhaomt454@gmail.com>
2014-07-03 19:52:59 -0400

    added new index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> xkcd Password Generator </title>
+	<meta charset = "utf-8">
+	<link rel = "stylesheet" href = "style.css" type = "css">
+</head>
+
+<body>
+
+
+</body>
+</html>
