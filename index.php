00efd9526d55ec1cfb9d47c22bb8e21122081803
wilzhao <wzhaomt454@gmail.com>
2014-07-04 12:48:39 -0400

    updated index.php, added a few more features and a little styling

--- index.php
+++ index.php
@@ -8,14 +8,23 @@
 </head>
 
 <body>
-	Password: <?php echo getRandomStringOfWords($_POST[numwords]);?>
+	<h1>
+	Password: <?php echo getPassword($_POST[numWords],
+									$_POST[includeNumber],
+   									$_POST[includeSymbol],
+   									$_POST[numSymbols],
+ 									$_POST[capitalize]);?>
+	</h1>	
 	<br>
-
-	Fill out the form for a password!
+	<br>
+	Fill out the form for an xkcd-esque password!
 	<form name = "input" action = "index.php" method = "POST">	
-		Number of words: <input type = "text" name = "numwords"></input><br>
-		<input type = "submit"></input>
+		Number of words: <input type = "text" name = "numWords"><br>
+		Include a number: <input type = "checkbox" name = "includeNumber"><br>
+		Include a special symbol: <input type = "checkbox" name = "includeSymbol"> How many symbols? <input type = "text" name = "numSymbols"> <br>
+		Capitalize first letter of every word: <input type = "checkbox" name = "capitalize"><br>
+		<input type = "submit">
 	</form>
-	
+
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 $words[0] = "word";
 $words[1] = "words";
 $words[2] = "bracket";
@@ -9,17 +9,50 @@ $words[5] = "flexibility";
 $words[6] = "abase";
 $words[7] = "nightmare";
 $words[8] = "little";
+*/
+$words = file("english-words.10");
 
-function getRandomWord(){
-	global $words;
-	return $words[rand(0,count($words))];
+$symbols = Array("!","@","#","$","%","^","&","*","(",")","~","`");
+function getRandomValueFromArray($array){
+	return $array[rand(0,count($array))];
 }
 
-function getRandomStringOfWords($numWords){
-	
+function getStringOfWords($numWords,$capitalize){
+	global $words;
 	while ($numWords > 0){
-		$wordString.= getRandomWord()." ";
+		$wordToAdd = getRandomValueFromArray($words);
+		if ($capitalize == "on"){
+			$wordToAdd = ucfirst($wordToAdd);
+		}
+		$wordString.= $wordToAdd." ";
 		$numWords--;
 	}
+	
 	return $wordString;
+}
+
+function getNumber($includeNumber){
+	if ($includeNumber == "on"){
+		return rand(0,9);
+	}
+}
+
+function getSymbols($includeSymbol,$numSymbols){
+	global $symbols;
+	if ($includeSymbol == "on"){
+		while ($numSymbols > 0){
+			$symbolString .= getRandomValueFromArray($symbols);
+			$numSymbols--;
+		}
+	}
+	return $symbolString;
+}
+
+function getPassword($numWords,$includeNumber,$includeSymbol,$numSymbols,$capitalize){
+	global $words,$symbols;
+	$password = getStringOfWords($numWords,$capitalize);
+	$password .= getNumber($includeNumber);
+	$password .= getSymbols($includeSymbol,$numSymbols);
+
+	return $password;
 }
